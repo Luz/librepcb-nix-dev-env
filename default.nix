c3c33c60eda9c8a63345160f53a29974ba4d611e
Luz <luz666@daum.net>
2023-09-18 20:13:31 +0200

    Use nativeBuildInputs where appropriate

--- default.nix
+++ default.nix
@@ -2,7 +2,9 @@ with import <nixpkgs> {};
 stdenv.mkDerivation rec {
   name = "env";
   env = buildEnv { name = name; paths = buildInputs; };
-  buildInputs = [
-    cmake libsForQt5.qt5.qtbase libsForQt5.qt5.qttools libsForQt5.qtquickcontrols2 opencascade-occt libGLU freeimage
+  nativeBuildInputs = [
+    cmake libsForQt5.qt5.qttools libsForQt5.qt5.wrapQtAppsHook libsForQt5.qtquickcontrols2 opencascade-occt libGLU
   ];
+  buildInputs = [ libsForQt5.qt5.qtbase ];
+  propagatedBuildInputs = [ freeimage ];
 }
