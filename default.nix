04c6aa779dd5bb3366c1918522501ffd4a44dc57
Luz <luz666@daum.net>
2023-09-18 20:14:30 +0200

    Fix qt.qpa.plugin: Could not find the Qt platform plugin xcb...

--- default.nix
+++ default.nix
@@ -7,4 +7,5 @@ stdenv.mkDerivation rec {
   ];
   buildInputs = [ libsForQt5.qt5.qtbase ];
   propagatedBuildInputs = [ freeimage ];
+  QT_QPA_PLATFORM_PLUGIN_PATH="${qt5.qtbase.bin}/lib/qt-${qt5.qtbase.version}/plugins";
 }
