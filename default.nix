6492af49164465189c84209dab546819446ccb49
Luz <luz666@daum.net>
2025-03-24 18:52:58 +0100

    Add qtsvg as in version 1.2.0 probably needed

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@ stdenv.mkDerivation rec {
   name = "env";
   env = buildEnv { name = name; paths = buildInputs; };
   nativeBuildInputs = [
-    cmake libsForQt5.qt5.qttools libsForQt5.qt5.wrapQtAppsHook libsForQt5.qtquickcontrols2 opencascade-occt libGLU
+    cmake libsForQt5.qt5.qttools libsForQt5.qt5.wrapQtAppsHook libsForQt5.qtsvg opencascade-occt libGLU
   ];
   buildInputs = [ libsForQt5.qt5.qtbase ];
   QT_QPA_PLATFORM_PLUGIN_PATH="${qt5.qtbase.bin}/lib/qt-${qt5.qtbase.version}/plugins";
