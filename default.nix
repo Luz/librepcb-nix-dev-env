53f26c8edcda4e41d7cf1a73b7db8cfd72b25c2e
Luz <luz666@daum.net>
2025-03-24 19:06:54 +0100

    Add rustc as needed for 1.3 and later

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@ stdenv.mkDerivation rec {
   name = "env";
   env = buildEnv { name = name; paths = buildInputs; };
   nativeBuildInputs = [
-    cmake pkgs.qt6.qttools pkgs.qt6.wrapQtAppsHook pkgs.qt6.qtsvg opencascade-occt libGLU
+    cmake pkgs.qt6.qttools pkgs.qt6.wrapQtAppsHook pkgs.qt6.qtsvg opencascade-occt libGLU rustc
   ];
   buildInputs = [ pkgs.qt6.qtbase ];
 #  QT_QPA_PLATFORM_PLUGIN_PATH="${qt5.qtbase.bin}/lib/qt-${qt5.qtbase.version}/plugins";
