564bc727cdef91b417ce1aa7e66cc0a124a12eeb
Luz <luz666@daum.net>
2025-03-24 18:46:26 +0100

    Remove freeimage, not needed since 1.1.0

--- default.nix
+++ default.nix
@@ -6,6 +6,5 @@ stdenv.mkDerivation rec {
     cmake libsForQt5.qt5.qttools libsForQt5.qt5.wrapQtAppsHook libsForQt5.qtquickcontrols2 opencascade-occt libGLU
   ];
   buildInputs = [ libsForQt5.qt5.qtbase ];
-  propagatedBuildInputs = [ freeimage ];
   QT_QPA_PLATFORM_PLUGIN_PATH="${qt5.qtbase.bin}/lib/qt-${qt5.qtbase.version}/plugins";
 }
